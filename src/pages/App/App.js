a0fda2143e2a851afc236cc953d484ff9a394a08
Kimberly Lord <kalord93@gmail.com>
2020-07-14 16:52:00 -0600

    Set up Switch and Route

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -47,6 +47,16 @@ class App extends Component {
             }
           </nav>
         </header>
+        <main>
+          <Switch>
+            <Route exact path='/signup' render={({ history }) =>
+              <SignupPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />
+            } />
+            <Route exact path='/login' render={({ history }) =>
+              <LoginPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />
+            } />
+          </Switch>
+        </main>
       </div>
     );
   }
