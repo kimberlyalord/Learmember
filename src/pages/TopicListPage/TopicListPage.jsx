e3665d223f063953f1cabaa739288f72a4d6070f
Kimberly Lord <kalord93@gmail.com>
2020-07-17 15:51:44 -0600

    add handleDeleteTopic to TopicListPage

--- src/pages/TopicListPage/TopicListPage.jsx
+++ src/pages/TopicListPage/TopicListPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './TopicListPage.css';
 import TopicCard from '../../components/TopicCard/TopicCard';
 
-function TopicListPage({ topics }) {
+function TopicListPage({ topics, handleDeleteTopic }) {
   return (
     <>
       <h1>Your Learning Topics</h1>
@@ -11,6 +11,7 @@ function TopicListPage({ topics }) {
           <TopicCard
             key={topic._id}
             topic={topic}
+            handleDeleteTopic={handleDeleteTopic}
           />
           )}
       </div>
